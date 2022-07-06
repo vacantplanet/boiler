9f9d125b7ebd0f1e33276a7029a8f6efadeb13c7
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-06 14:43:00 +0200

    Add Sanitizer

---
+++ src/Sanitizer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Boiler;
+
+use Symfony\Component\HtmlSanitizer\{
+    HtmlSanitizer,
+    HtmlSanitizerConfig,
+};
+
+
+class Sanitizer
+{
+    public static function clean(
+        string $html,
+        ?HtmlSanitizerConfig $config = null,
+        bool $removeEmptyLines = true,
+    ): string {
+        $config = $config ?: (new HtmlSanitizerConfig())
+            // Allow "safe" elements and attributes. All scripts will be removed
+            // as well as other dangerous behaviors like CSS injection
+            ->allowSafeElements();
+        $sanitizer = new HtmlSanitizer($config);
+        $result = $sanitizer->sanitize($html);
+
+        // also remove empty lines
+        return $removeEmptyLines ?
+            preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", PHP_EOL, $result) :
+            $result;
+    }
+}
