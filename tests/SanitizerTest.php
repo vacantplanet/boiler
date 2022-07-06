9f6ba4a9b759c7a2ce59eda94d8b7e0e3bf0c1ea
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-06 15:53:46 +0200

    Add Sanitizer tests

---
+++ tests/SanitizerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
+use Conia\Boiler\Sanitizer;
+
+const MALFORMED = '
+        <header>Test</header>
+        <aside><div>Test</div></aside>
+        <iframe src="example.com"></iframe>
+        <nav><ul><li>Test</li></ul></nav>
+        <article>
+            <script>console.log("hans");</script>
+            <section>
+                <h1 onclick="console.log("hans");">Test</h1>
+            </section>
+        </article>
+        <footer>Test</footer>';
+
+
+test('Clean with config', function () {
+    $clean = '
+        <header>Test</header>
+        <aside><div>Test</div></aside>
+        <nav><ul><li>Test</li></ul></nav>
+        <article>
+            <section>
+                <h1>Test</h1>
+            </section>
+        </article>
+        <footer>Test</footer>';
+
+    expect(Sanitizer::clean(MALFORMED))->toBe($clean);
+});
+
+
+test('Clean with block extension', function () {
+    $config = (new HtmlSanitizerConfig())
+        ->allowSafeElements()
+        ->blockElement('header')
+        ->blockElement('footer')
+        ->blockElement('section');
+    $clean = '
+        Test
+        <aside><div>Test</div></aside>
+        <nav><ul><li>Test</li></ul></nav>
+        <article>
+                <h1>Test</h1>
+        </article>
+        Test';
+
+    expect(Sanitizer::clean(MALFORMED, $config))->toBe($clean);
+});
