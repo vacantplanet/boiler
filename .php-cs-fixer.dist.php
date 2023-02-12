0da2547bf6ae26d64a29abbf022f29ef1c4879a8
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-02-12 14:16:14 +0100

    Leave safe scalar values passed to context as they are

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,30 +3,24 @@
 $finder = PhpCsFixer\Finder::create();
 $config = new PhpCsFixer\Config();
 
-
 return $config->setRules([
     '@PSR12' => true,
     '@PhpCsFixer' => true,
     'array_indentation' => true,
     'array_syntax' => ['syntax' => 'short'],
+    'blank_line_before_statement' => ['statements' => ['continue', 'declare', 'return', 'throw', 'try']],
     'braces' => true,
     'cast_spaces' => ['space' => 'none'],
+    'class_attributes_separation' => ['elements' => ['case' => 'none', 'property' => 'none']],
     'class_definition' => ['space_before_parenthesis' => true],
     'concat_space' => ['spacing' => 'one'],
-    'echo_tag_syntax' => [
-        'format' => 'short',
-        'shorten_simple_statements_only' => true,
-    ],
-    'global_namespace_import' => [
-        'import_classes' => true,
-        'import_constants' => null,
-        'import_functions' => null,
-    ],
+    'echo_tag_syntax' => [ 'format' => 'short', 'shorten_simple_statements_only' => true ],
+    'global_namespace_import' => [ 'import_classes' => true, 'import_constants' => null, 'import_functions' => null ],
     'increment_style' => ['style' => 'post'],
     'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
     'multiline_comment_opening_closing' => false,
     'multiline_whitespace_before_semicolons' => false,
-    'no_extra_blank_lines' => false,
+    'no_alternative_syntax' => ['fix_non_monolithic_code' => false],
     'no_unused_imports' => false,
     'no_whitespace_before_comma_in_array' => true,
     'phpdoc_to_comment' => false,
--- src/TemplateContext.php
+++ src/TemplateContext.php
@@ -45,6 +45,14 @@ class TemplateContext
                     }
                 }
 
+                if (is_scalar($value) && !is_string($value)) {
+                    return $value;
+                }
+
+                if (is_null($value)) {
+                    return null;
+                }
+
                 return Wrapper::wrap($value);
             },
             array_merge($this->context, $values)
