1add6572597841f5defa1837dfeb34ed3c6ddedc
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-03-07 16:53:08 +0100

    Remove Chuck renderer

--- src/Exception/RendererException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Conia\Boiler\Exception;
-
-class RendererException extends RuntimeException implements TemplateException
-{
-}
--- tests/templates/default/renderer.php
+++
@@ -1,4 +0,0 @@
-<h1><?php echo $config->app(); ?></h1>
-<p><?php echo $text; ?></p><?php foreach ($arr as $num) {
-    ?><p><?php echo $num; ?></p><?php
-} ?>
