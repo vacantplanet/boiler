29140ef71332d1c13d49e8bd06500b0ab88ef316
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-08-15 15:35:10 +0200

    Transform UrlTest from pest to PHPUnit format

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -2,27 +2,38 @@
 
 declare(strict_types=1);
 
+namespace VacantPlanet\Boiler\Tests;
+
 use VacantPlanet\Boiler\Exception\RuntimeException;
 use VacantPlanet\Boiler\Url;
 
-test('All segments', function () {
-	$url = Url::clean(
-		"http://user :pss@example.com:81/mypath/   " .
-		" myfile.html?a=\"chuck schuldiner\"&b[]=2&b[]\n\n\n=3&z=666#symbolic",
-	);
+final class UrlTest extends TestCase
+{
+	public function testAllSegments(): void
+	{
+		$url = Url::clean(
+			"http://user :pss@example.com:81/mypath/   " .
+			" myfile.html?a=\"chuck schuldiner\"&b[]=2&b[]\n\n\n=3&z=666#symbolic",
+		);
 
-	expect($url)->toBe(
-		'http://user%20%3Apss@example.com:81/mypath/++++myfile.html' .
-		'?a=%22chuck+schuldiner%22&b%5B0%5D=2&b%5B1%5D=3&z=666#symbolic',
-	);
-});
+		$this->assertSame(
+			'http://user%20%3Apss@example.com:81/mypath/++++myfile.html' .
+				'?a=%22chuck+schuldiner%22&b%5B0%5D=2&b%5B1%5D=3&z=666#symbolic',
+			$url,
+		);
+	}
 
-test('Already clean without anything', function () {
-	$url = Url::clean('http://example.com/');
+	public function testAlreadyCleanWithoutAnything(): void
+	{
+		$url = Url::clean('http://example.com/');
 
-	expect($url)->toBe('http://example.com/');
-});
+		$this->assertSame('http://example.com/', $url);
+	}
 
-test('Failing', function () {
-	Url::clean('scheme://example_login:!#Password?@ZZZ@127.0.0.1/some_path');
-})->throws(RuntimeException::class, 'Invalid Url');
+	public function testFailedParsing(): void
+	{
+		$this->throws(RuntimeException::class, 'Invalid Url');
+
+		Url::clean('scheme://example_login:!#Password?@ZZZ@127.0.0.1/some_path');
+	}
+}
